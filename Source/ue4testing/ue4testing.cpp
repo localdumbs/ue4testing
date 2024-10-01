ef10ab0c7e1c65aa99caaf60733750a171edd823
local_dumb <113314528+localdumbs@users.noreply.github.com>
2024-09-30 23:47:26 -0400

    umm thingy i geuss

---
+++ Source/ue4testing/ue4testing.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ue4testing.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue4testing, "ue4testing" );
